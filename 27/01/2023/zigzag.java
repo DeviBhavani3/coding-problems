6ea4f2facb3d80248e39279f7cafb76272565efe
Devi Bhavani Annapareddy <85300128+DeviBhavani3@users.noreply.github.com>
2023-01-27 05:08:18 -0800

    Create zigzag.java

---
+++ 27/01/2023/zigzag.java
@@ -0,0 +1,42 @@
+class Solution {
+    public List<List<Integer>> zigzagLevelOrder(TreeNode root) {
+        List<List<Integer>> list = new ArrayList<List<Integer>>();
+        zigzag(root, list);
+        return list;
+    }
+    public void zigzag(TreeNode root, List<List<Integer>> list){
+        if(root==null) 
+          return;
+        Stack<TreeNode> s1 = new Stack<TreeNode>();
+        Stack<TreeNode> s2 = new Stack<TreeNode>();
+        s1.push(root);
+        while(s1.isEmpty()==false || s2.isEmpty()==false){
+            List<Integer> res1 = new ArrayList<Integer>();
+            while(s1.isEmpty()==false){
+                TreeNode curr = s1.pop();
+                if(curr.left!=null) 
+		            s2.push(curr.left);
+                if(curr.right!=null)
+		            s2.push(curr.right);
+                res1.add(curr.val);
+            }
+            System.out.println("res1 " + res1);
+            list.add(res1);
+            if(!s1.isEmpty()==false && !s2.isEmpty()==false){
+                break;
+            }
+            List<Integer> res2 = new ArrayList<Integer>();
+            while(s2.isEmpty()==false)
+            {
+                TreeNode curr = s2.pop();
+                if(curr.right!=null) 
+			          s1.push(curr.right);
+                if(curr.left!=null)  
+                s1.push(curr.left);
+                res2.add(curr.val);
+            }
+            System.out.println("res2  " + res2);
+            list.add(res2);
+        }
+    }
+}
