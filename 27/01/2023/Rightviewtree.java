57902badebcde1db9810d123524cc776a97d37d8
Devi Bhavani Annapareddy <85300128+DeviBhavani3@users.noreply.github.com>
2023-01-27 06:29:39 -0800

    Create Rightviewtree.java

---
+++ 27/01/2023/Rightviewtree.java
@@ -0,0 +1,30 @@
+class Solution {
+    public List<Integer> rightSideView(TreeNode root) {
+        Queue <TreeNode> queue = new LinkedList<>();
+        ArrayList<Integer> list =new ArrayList<Integer>();
+        if(root==null)
+        {
+            return list;
+        }
+        queue.add(root);
+        while(!queue.isEmpty()){
+            int size = queue.size();
+            for(int i=0;i<size;i++)
+            {
+              TreeNode curr =queue.poll();
+              if(i==0){
+                  list.add(curr.val);
+              }
+              if(curr.right!=null){
+                  queue.add(curr.right);
+              }
+              if(curr.left!=null){
+                  queue.add(curr.left);
+              }
+            }
+        }
+        return list;
+
+        
+    }
+}
