3eb5ca1a9c26ca20484b257f7c0aa2ae1d88ddeb
Devi Bhavani Annapareddy <85300128+DeviBhavani3@users.noreply.github.com>
2023-01-28 00:47:13 -0800

    Update SymmetricTree.java

--- 26/01/2023/SymmetricTree.java
+++ 26/01/2023/SymmetricTree.java
@@ -1,3 +1,15 @@
+public class TreeNode {
+      int val;
+      TreeNode left;
+      TreeNode right;
+      TreeNode() {}
+      TreeNode(int val) { this.val = val; }
+      TreeNode(int val, TreeNode left, TreeNode right) {
+          this.val = val;
+          this.left = left;
+          this.right = right;
+      }
+  }
 class Solution {
     public boolean isSymmetric(TreeNode root) {
          return root == null || isMirror(root.left, root.right);
