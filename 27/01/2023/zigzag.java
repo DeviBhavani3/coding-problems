703143d667f8dc6e6aca552ff4384d67a02d4696
Devi Bhavani Annapareddy <85300128+DeviBhavani3@users.noreply.github.com>
2023-01-28 00:55:11 -0800

    Update zigzag.java

--- 27/01/2023/zigzag.java
+++ 27/01/2023/zigzag.java
@@ -1,3 +1,15 @@
+public class TreeNode {
+     int val;
+      TreeNode left;
+      TreeNode right;
+      TreeNode() {}
+     TreeNode(int val) { this.val = val; }
+     TreeNode(int val, TreeNode left, TreeNode right) {
+          this.val = val;
+          this.left = left;
+          this.right = right;
+      }
+  }
 class Solution {
     public List<List<Integer>> zigzagLevelOrder(TreeNode root) {
         List<List<Integer>> list = new ArrayList<List<Integer>>();
