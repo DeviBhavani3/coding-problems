0eb23b2cc1cedaa895f4924a57721c450c03b129
Devi Bhavani Annapareddy <85300128+DeviBhavani3@users.noreply.github.com>
2023-02-05 09:54:01 -0800

    Create MinimumDistanceBetweenBSTNodes.java

---
+++ 05/02/2023/MinimumDistanceBetweenBSTNodes.java
@@ -0,0 +1,41 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    ArrayList<Integer> list = new ArrayList<>();
+    public int minDiffInBST(TreeNode root) {
+        if(root==null){
+            return 0;
+        }
+        inorder(root,list);
+        int min = Integer.MAX_VALUE;
+        for(int i=0;i<list.size()-1;i++){
+            if(list.get(i+1)-list.get(i)<min){
+                min = list.get(i+1)-list.get(i);
+            }
+        }
+        return min;
+        
+    }
+    public void inorder(TreeNode root, ArrayList<Integer>list){
+        if(root==null){
+            return;
+        }
+        inorder(root.left,list);
+        list.add(root.val);
+        inorder(root.right,list);
+    }
+    
+}
